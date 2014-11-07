f2a5425013a1b05a329616484a58bee3a14bed5a
Jayson <chenjiazhan@hotmail.com>
2014-11-07 21:50:03 +0800

    code

--- src/main/webapp/js/gallery.js
+++ src/main/webapp/js/gallery.js
@@ -1,19 +1,20 @@
 function Gallery() {
     var timer;
     var items;
-    var count = 0;
+    var count;
 }
 Gallery.prototype.init=function(){
     var itemCount = $('.gallery .item').length;
     var itemWidth = $('.gallery .item').width();
     $('.gallery ul').css("width" , (itemCount * itemWidth) + 'px');
-    this.items = $('.gallery');
+    this.items = $('.gallery ul');
+    this.count = 0;
 };
 Gallery.prototype.start= function () {
-    this.timer = setInterval("gallery.start();" , 1000);
+    this.timer = setInterval("gallery.slide();" , 1000);
 }
 Gallery.prototype.slide= function () {
-    var marginLeft = this.count%5*1000 + "px";
+    var marginLeft = "-" + this.count%5*1000 + "px";
     this.items.css("margin-left" , marginLeft);
     this.count = this.count + 1;
 }
